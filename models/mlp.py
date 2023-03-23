88730f7ccf94f7dd2f313fd577e216c3411c2028
Smriti Suresh <smritis@bu.edu>
2023-03-23 02:07:01 -0400

    MLP hidden-layer1 complete

--- models/mlp.py
+++ models/mlp.py
@@ -1,3 +1,5 @@
+#A Multilayer perceptron to compute weights of each of the AI detection models according to the outputs they predicted
+
 import numpy as np
 import pandas as pd
 from sklearn.neural_network import MLPRegressor
@@ -12,7 +14,7 @@ y_true = data.iloc[:, 3].values
 print(y_true)
 
 # Define an MLPRegressor to learn the weights of the models
-mlp = MLPRegressor(hidden_layer_sizes=(10,), max_iter=1000)
+mlp = MLPRegressor(hidden_layer_sizes=(1,), max_iter=1000)
 
 # Fit the MLPRegressor on the predictions and ground truth
 mlp.fit(y_pred, y_true)
@@ -22,7 +24,11 @@ weights = mlp.coefs_[0]
 
 # Normalize the weights to sum up to 1
 weights /= np.sum(weights)
-print(weights)
-# Write the weights to a new CSV file
-weights_df = pd.DataFrame(weights, columns=['Weights'])
-weights_df.to_csv('model_weights.csv', index=False)
+print("Weights obtained:\n")
+print("Open AI Classifier:", weights[0][0])
+print("Zero GPT:", weights[1][0])
+print("GPT Zero:", weights[2][0])
+print("\n")
+y_combined = np.dot(y_pred, weights)
+print("Predicted labels:\n", y_combined)
+# print(len(y_combined))
