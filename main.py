84e7d8cc324b79a7d35fa6fa9c5c0f9373c9c5b2
Gitika Jha <gitijh@gmail.com>
2023-04-18 23:04:15 -0400

    adding main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+import csv
+import os
+from gptzero import GPTZeroAPI
+
+api_key = '6341e691c2b8492baef3035fec80d31a' # replace API key
+gptzero_api = GPTZeroAPI(api_key)
+
+input_csv_path = './data/data.csv'
+output_csv_path = './data/results.csv' # keep replacing this if you want to preserve all data
+txt_files_dir = './data/txtfiles/'
+
+if not os.path.exists(txt_files_dir):
+    os.makedirs(txt_files_dir, mode=0o777)
+
+with open(input_csv_path, 'r', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+
+    next(reader)
+
+    results = []
+    for row in reader:
+        row_id = row[0]
+        row_content = row[1]
+
+        filename = f"{row_id}.txt"
+        file_path = os.path.join(txt_files_dir, filename)
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write(row_content)
+
+        
+        with open(file_path, 'r', encoding='utf-8') as f:
+            content = f.read()
+
+            response = gptzero_api.text_predict(content)
+
+            if 'error' in response:
+                print('error')
+                continue
+
+            sentences = response['documents'][0]['sentences']
+            perplexities = [sentence['perplexity'] for sentence in sentences]
+            avg_perplexity = sum(perplexities) / len(perplexities)
+            
+            avg_gen_prob = response['documents'][0]['average_generated_prob']
+            comp_gen_prob = response['documents'][0]['completely_generated_prob']
+            overall_burstiness = response['documents'][0]['overall_burstiness']
+            
+            gptzero_prediction = "AI" if comp_gen_prob > 0.65 else "Human"
+
+            results.append([row_id, row_content, avg_gen_prob, comp_gen_prob, overall_burstiness, avg_perplexity, gptzero_prediction])
+
+with open(output_csv_path, 'w', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f)
+    writer.writerow(['id', 'truncated', 'Average Generated Probability', 'Completely Generated Probability', 'Overall Burstiness', 'Average Perplexity', 'GPTZero Prediction'])
+    writer.writerows(results)
